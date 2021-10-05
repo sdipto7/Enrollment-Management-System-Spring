137fb68e12f99e6079d44ace386975cdbd969b22
sdipto7 <rumi.dipto@therapservices.net>
2021-10-05 15:20:48 +0600

    Added error checking after validation of credential in LoginController class

--- src/main/java/net/therap/enrollmentmanagement/controller/LoginController.java
+++ src/main/java/net/therap/enrollmentmanagement/controller/LoginController.java
@@ -6,6 +6,7 @@ import net.therap.enrollmentmanagement.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,22 +25,30 @@ public class LoginController {
 
     @Autowired
     private UserService userService;
+    private static final String LOGIN_PAGE = "login";
+    private static final String HOME_PAGE = "home";
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String viewLoginPage(ModelMap model) {
         model.addAttribute("credential", new Credential());
 
-        return "login";
+        return LOGIN_PAGE;
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(@Valid @ModelAttribute Credential credential, ModelMap model) {
+    public String login(@Valid @ModelAttribute Credential credential,
+                        Errors errors,
+                        ModelMap model) {
+        if (errors.hasErrors()) {
+            return LOGIN_PAGE;
+        }
+
         User user = userService.findByCredential(credential);
         if (Objects.nonNull(user)) {
             model.addAttribute("currentUser", user);
-            return "home";
+            return HOME_PAGE;
         } else {
-            return "login";
+            return LOGIN_PAGE;
         }
     }
 }
